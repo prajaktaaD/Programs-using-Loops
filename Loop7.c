79ea0a6b0a364f5066bb66dfa70237842b7d0180
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-08 21:01:53 +0530

    Create Loop7.c
    Find square of the number without using * operator

---
+++ Loop7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,num,sq;
+    
+    printf("Enter the value of num:");
+    scanf("%d",&num);
+    for(i=0;i<num;i++)
+       sq=sq+num;
+         
+    printf("The square of numbers:%d",sq);
+     
+    
+    return 0;
+}
