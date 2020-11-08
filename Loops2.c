87db7f61ad82b08066fb776ed02cef30106a65b9
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-08 19:03:06 +0530

    Create Loops2.c
    Print first 10 odd numbers.

---
+++ Loops2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("First 10 odd numbers is:");
+    
+    for(i=1;i<=20;)
+    {
+        printf("%d ",i);
+        i=i+2;
+    }    
+        
+    
+    return 0;
+}
