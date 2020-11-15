5ea3fdb55ce070aaad1943d52898cd92f8b4a928
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-15 18:29:49 +0530

    Update Loops10.c

--- Loops10.c
+++ Loops10.c
@@ -10,7 +10,10 @@ int main()
     for(i=2;i<num;i++)
     {
         if(num%i==0)
-        f=1;
+        {
+            f=1;
+            break;
+        }         
         
         else
         f=0;
